764f17367939a0069f61ce622f53380066ee2cbf
kcsheng <kacysheng@mail.com>
2021-07-22 11:09:33 +1000

    Reorganise functions and add license badge functionality

--- readme/README.md
+++ readme/README.md
@@ -1,32 +1,36 @@
+
 <style>
   h2, h3 {
-    padding-bottom: 5px;
     border-bottom: 1px solid #d3d3d3;
+    padding-bottom: 5px;
   }
 </style>
-<h2>asdfadgf</h2>
--->badges here <br>
-rwyrtyer
+<h2>sdfyeutu</h2>
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+<h3>Description</h3>
+etyure
 <h3>Table of Contents</h3>
 <ul>
   <li><a href="#install">Installation</a></li>
-  <li><a href="#usage">Usage information</a></li>
-  <li><a href="#contribution">Contribution guidelines</a></li>
-  <li><a href="#test">Test instructions</a></li>
+  <li><a href="#usage">Usage</a></li>
+  <li><a href="#contribution">Contributing</a></li>
+  <li><a href="#test">Tests</a></li>
   <li><a href="#license">License</a></li>
-  <li><a href="#contact">Contact</a></li>
+  <li><a href="#contact">Questions</a></li>
 </ul>
 <h3 id="install">Installation</h3>
-yjrtuetyu
+ertyeuy
 <h3 id="usage">Usage Information</h3>
-ertuteeye
+retuy
 <h3 id="contribution">Contribution guidelines</h3>
-etrutu
+rteuiys
 <h3 id="test">Test instructions</h3>
-dfhgfsdgjd
+sfdghjt
 <h3 id="license">License</h3>
-MIT
+Apache
 <h3 id="contact">Contact</h3>
 Contact me if you have any questions:<br>
-My guthub link: <a href="https://www.github.com/sfhgdfgjg">https://www.github.com/sfhgdfgjg</a><br>
-My email: sfdghdgj
+My guthub link: <a href="https://www.github.com/fghdjh">https://www.github.com/fghdjh</a><br>
+My email: tryuri
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,88 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Returns a license badge according to the license input
+function renderLicenseBadge(license) {
+  const licenseBadges = {
+    MIT: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]",
+    Apache:
+      "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]",
+    GPLv2:
+      "[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)]",
+    GPLv3:
+      "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]",
+    "BSD 2-clause":
+      "[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)]",
+    "BSD 3-clause":
+      "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]",
+    LGPLv3:
+      "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)]",
+    AGPLv3:
+      "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)]",
+    Unlicense:
+      "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]",
+  };
+  return license in licenseBadges ? licenseBadges[license] : "";
+}
+// Returns a license link according to the license input
+function renderLicenseLink(license) {
+  const licenseLinks = {
+    MIT: "https://opensource.org/licenses/MIT",
+    Apache: "https://opensource.org/licenses/Apache-2.0",
+    GPLv2: "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html",
+    GPLv3: "https://www.gnu.org/licenses/gpl-3.0",
+    "BSD 2-clause": "https://opensource.org/licenses/BSD-2-Clause",
+    "BSD 3-clause": "https://opensource.org/licenses/BSD-3-Clause",
+    LGPLv3: "https://www.gnu.org/licenses/lgpl-3.0",
+    AGPLv3: "https://www.gnu.org/licenses/agpl-3.0",
+    Unlicense: "http://unlicense.org/",
+  };
+  return license in licenseLinks ? licenseLinks[license] : "";
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Returns the badge with anchored link
+function renderLicenseSection(license) {
+  return license
+    ? `${renderLicenseBadge(license)}(${renderLicenseLink(license)})`
+    : "";
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const license = data.license;
+  const licenseInfo = renderLicenseSection(license);
+  return `
+<style>
+  h2, h3 {
+    border-bottom: 1px solid #d3d3d3;
+    padding-bottom: 5px;
+  }
+</style>
+<h2>${data.project_title}</h2>
 
+${licenseInfo}
+
+<h3>Description</h3>
+${data.project_descr}
+<h3>Table of Contents</h3>
+<ul>
+  <li><a href="#install">Installation</a></li>
+  <li><a href="#usage">Usage</a></li>
+  <li><a href="#contribution">Contributing</a></li>
+  <li><a href="#test">Tests</a></li>
+  <li><a href="#license">License</a></li>
+  <li><a href="#contact">Questions</a></li>
+</ul>
+<h3 id="install">Installation</h3>
+${data.installation}
+<h3 id="usage">Usage Information</h3>
+${data.usage_info}
+<h3 id="contribution">Contribution guidelines</h3>
+${data.guidelines}
+<h3 id="test">Test instructions</h3>
+${data.test}
+<h3 id="license">License</h3>
+${data.license}
+<h3 id="contact">Contact</h3>
+Contact me if you have any questions:<br>
+My guthub link: <a href="https://www.github.com/${data.username}">https://www.github.com/${data.username}</a><br>
+My email: ${data.email}
 `;
 }
 
